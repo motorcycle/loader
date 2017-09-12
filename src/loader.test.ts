7964fcac8aed98e062433c1bc7632a53cd654e32
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-09-11 21:28:14 -0400

    fix: test error

--- src/loader.test.ts
+++ src/loader.test.ts
@@ -41,17 +41,21 @@ export const test: Test = describe(`loader`, [
 
       const expectedCode = `"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var stream_1 = require("@motorcycle/stream");
 var mostly_dom_1 = require("@motorcycle/mostly-dom");
 var UI_1 = require("./UI");
 var element = document.querySelector('#app');
 if (!element)
     throw new Error('could not find element');
-var sinkProxies__generated__by__motorcycle__compiler = {};
+var sinkProxies__generated__by__motorcycle__compiler = {
+    view$: stream_1.createProxy().stream
+};
 var sources__generated__by__motorcycle__compiler = mostly_dom_1.makeDomComponent(element)(sinkProxies__generated__by__motorcycle__compiler);
 var sinks__generated__by__motorcycle__compiler = UI_1.UI(sources__generated__by__motorcycle__compiler);
+sinks__generated__by__motorcycle__compiler.view$.run(sinkProxies__generated__by__motorcycle__compiler.view$, stream_1.scheduler);
 //# sourceMappingURL=module.js.map`
 
-      const expectedSourceMap = `{"version":3,"file":"module.js","sourceRoot":"","sources":["module.ts"],"names":[],"mappings":";;AACA,qDAAgF;AAChF,2BAA0B;AAC1B,IAAM,OAAO,GAAG,QAAQ,CAAC,aAAa,CAAC,MAAM,CAAC,CAAC;AAC/C,EAAE,CAAC,CAAC,CAAC,OAAO,CAAC;IACT,MAAM,IAAI,KAAK,CAAC,wBAAwB,CAAC,CAAC;AAC9C,IAAM,gDAAgD,GAAG,EAAE,CAAC;AAC5D,IAAM,4CAA4C,GAAG,6BAAgB,CAAC,OAAO,CAAC,CAAC,gDAAgD,CAAC,CAAC;AACjI,IAAM,0CAA0C,GAAG,OAAE,CAAC,4CAA4C,CAAC,CAAC"}`
+      const expectedSourceMap = `{"version":3,"file":"module.js","sourceRoot":"","sources":["module.ts"],"names":[],"mappings":";;AAAA,6CAA4D;AAC5D,qDAAgF;AAChF,2BAA0B;AAC1B,IAAM,OAAO,GAAG,QAAQ,CAAC,aAAa,CAAC,MAAM,CAAC,CAAC;AAC/C,EAAE,CAAC,CAAC,CAAC,OAAO,CAAC;IACT,MAAM,IAAI,KAAK,CAAC,wBAAwB,CAAC,CAAC;AAC9C,IAAM,gDAAgD,GAAG;IACrD,KAAK,EAAE,oBAAW,EAAE,CAAC,MAAM;CAC9B,CAAC;AACF,IAAM,4CAA4C,GAAG,6BAAgB,CAAC,OAAO,CAAC,CAAC,gDAAgD,CAAC,CAAC;AACjI,IAAM,0CAA0C,GAAG,OAAE,CAAC,4CAA4C,CAAC,CAAC;AACpG,0CAA0C,CAAC,KAAK,CAAC,GAAG,CAAC,gDAAgD,CAAC,KAAK,EAAE,kBAAS,CAAC,CAAA"}`
 
       const callback: loader.AsyncCallBack = function callback(
         err: Error | null,
